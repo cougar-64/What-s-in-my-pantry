7111f3659864dc2547a7676062480b4a231ea6db
Samuel <samuelbird01@gmail.com>
2025-10-20 20:30:53 -0600

    password requirement deleted and username showing

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,21 +2,25 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export function Login() {
+   const [text, setText] = React.useState('');
+   function loginUser(){
+      localStorage.setItem('user', text);
+   }
+
+   function textChange(e) {
+      setText(e.target.value);
+   }
    return (
       <main>
          <h1>Welcome! Please login to get started, or create an account</h1>
          <form method="get" action="pantrys.html">
             <div>
                <span>Username</span>
-               <input type="text" placeholder="username" />
+               <input onChange={textChange} type="text" placeholder="username" />
 
             </div>
-            <div>
-               <span>Password</span>
-               <input type="password" plaeholder="********" />
-            </div>
             <li>
-               <NavLink to='/pantrys'><button type="button">Login</button></NavLink>
+               <NavLink to='/pantrys'><button onClick={loginUser} type="button">Login</button></NavLink>
                <NavLink to='/pantrys'><button type="button">Create Account</button></NavLink>
             </li>
          </form>
--- src/pantrys/pantrys.jsx
+++ src/pantrys/pantrys.jsx
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom';
 import './pantrys.css';
 
 export function Pantrys() {
+   const [user, setUser] = React.useState(localStorage.getItem('user') || null);
    return (
       <main>
       <h1 className="users">
-         User: Username from Database</h1>
+         User: {user}</h1>
       <ul className="notification">
          <li className="player-name">Websocket Notification - Johnny created a new pantry</li>
          <li className="player-name">Websocket Notification - Lisa joined 'Work' pantry</li>
