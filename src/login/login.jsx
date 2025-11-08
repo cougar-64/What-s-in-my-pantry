296f650dfb4f83655261df40bc9e319eb512bfa7
Samuel <samuelbird01@gmail.com>
2025-11-08 13:42:41 -0700

    http: updated to https:// in login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -7,7 +7,7 @@ export function Login({ setUser }) {
   const navigate = useNavigate();
 
   async function createUser() {
-    const response = await fetch('http://startup.byu260.click/api/auth/create', {
+    const response = await fetch('https://startup.byu260.click/api/auth/create', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
@@ -27,7 +27,7 @@ export function Login({ setUser }) {
   }
 
   async function loginUser() {
-   const response = await fetch('http://startup.byu260.click/api/auth/login', {
+   const response = await fetch('https://startup.byu260.click/api/auth/login', {
      method: 'POST',
      headers: { 'Content-Type': 'application/json' },
      credentials: 'include',  // send cookies
