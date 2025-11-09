8da6d4a5bdaf73cd8c65ece2440f0484750d32ad
Samuel <samuelbird01@gmail.com>
2025-11-09 01:07:25 -0700

    trying to connect immediately (AI suggest)

--- service/database.js
+++ service/database.js
@@ -1,11 +1,36 @@
+// const { MongoClient } = require('mongodb');
+// const config = require('./dbConfig.json');
+
+// const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+// const client = new MongoClient(url);
+// const db = client.db('startup');
+// const userCollection = db.collection('user');
+// const pantryCollection = db.collection('pantry');
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('startup');
-const userCollection = db.collection('user');
-const pantryCollection = db.collection('pantry');
+
+let db;
+let userCollection;
+let pantryCollection;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db('startup');
+    userCollection = db.collection('user');
+    pantryCollection = db.collection('pantry');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Unable to connect to MongoDB:', err);
+    process.exit(1);
+  }
+}
+
+connectDB();
+
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
