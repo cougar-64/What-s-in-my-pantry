f76dbf9bf3f5f03575762d1d27c0db779334d5d0
Samuel <samuelbird01@gmail.com>
2025-11-12 11:21:13 -0700

    useParams imported

--- src/specificPantry/specificPantry.jsx
+++ src/specificPantry/specificPantry.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
 
 export function SpecificPantry() {
   const { id } = useParams();
